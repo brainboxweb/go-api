c20a0d0d6f6b4514b9bf453758cc6392daca9f93
Gary Straughan <orders@brainboxweb.co.uk>
2015-10-29 16:23:59 +0000

    New endpoints: update, delete

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ Dependencies
 ---
 
         go get github.com/gin-gonic/gin
+        go get gopkg.in/gorp.v1
+        go get github.com/mattn/go-sqlite3
 
 
 Run
@@ -18,15 +20,28 @@ Run
         
 Test
 ----
-
-         curl http://localhost:8080/api/v1/users
-         //[{"id":1,"firstname":"Oliver","lastname":"Queen"},{"id":2,"firstname":"Malcom","lastname":"Merlyn"}]
+        //list
+        curl http://localhost:8080/api/v1/users
+        //[]
          
-         curl http://localhost:8080/api/v1/users/1
-         //{"id":1,"firstname":"Oliver","lastname":"Queen"}
+        //add 
+        curl -i -X POST -H "Content-Type: application/json" -d "{ \"firstname\": \"Thea\", \"lastname\": \"Queen\" }" http://localhost:8080/api/v1/users
          
-         curl http://localhost:8080/api/v1/users/2
-         //{"id":2,"firstname":"Malcom","lastname":"Merlyn"}
+        //list
+        curl http://localhost:8080/api/v1/users
+        //[{"id":1,"firstname":"Thea","lastname":"Queen"}]
          
-         
-         
+        //update
+        curl -i -X PUT -H "Content-Type: application/json" -d "{ \"firstname\": \"Thea\", \"lastname\": \"Merlyn\" }" http://localhost:8080/api/v1/users/1
+     
+        //list
+        curl http://localhost:8080/api/v1/users
+        //[{"id":1,"firstname":"Thea","lastname":"Merlyn"}]
+        
+        //delete
+        curl -i -X DELETE http://localhost:8080/api/v1/users/1
+        
+        //list
+        curl http://localhost:8080/api/v1/users
+        //[]
+        
--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
- "github.com/gin-gonic/gin"
- "strconv"
+	"strconv"
+	"database/sql"
+	"github.com/gin-gonic/gin"
+	_ "github.com/mattn/go-sqlite3"
+	"gopkg.in/gorp.v1"
+	"log"
 )
 
 type User struct {
-	Id int64 `db:"id" json:"id"`
+	Id        int64 `db:"id" json:"id"`
 	Firstname string `db:"firstname" json:"firstname"`
-	Lastname string `db:"lastname" json:"lastname"`
+	Lastname  string `db:"lastname" json:"lastname"`
 }
 
 func main() {
@@ -25,35 +29,118 @@ func main() {
 }
 
 func GetUsers(c *gin.Context) {
-	type Users []User
-	var users = Users{
-		User{Id: 1, Firstname: "Oliver", Lastname: "Queen"},
-		User{Id: 2, Firstname: "Malcom", Lastname: "Merlyn"},
+	var users []User
+	_, err := dbmap.Select(&users, "SELECT * FROM user")
+	if err == nil {
+		c.JSON(200, users)
+	} else {
+		c.JSON(404, gin.H{"error": "no user(s) into the table"})
 	}
-	c.JSON(200, users)
 	// curl -i http://localhost:8080/api/v1/users
 }
+
 func GetUser(c *gin.Context) {
 	id := c.Params.ByName("id")
-	user_id, _ := strconv.ParseInt(id, 0, 64)
-	if user_id == 1 {
-		content := gin.H{"id": user_id, "firstname": "Oliver", "lastname": "Queen"}
-		c.JSON(200, content)
-	} else if user_id == 2 {
-		content := gin.H{"id": user_id, "firstname": "Malcom", "lastname": "Merlyn"}
+	var user User
+	err := dbmap.SelectOne(&user, "SELECT * FROM user WHERE id=?", id)
+	if err == nil {
+		user_id, _ := strconv.ParseInt(id, 0, 64)
+		content := &User{
+			Id: user_id,
+			Firstname: user.Firstname,
+			Lastname: user.Lastname,
+		}
 		c.JSON(200, content)
 	} else {
-		content := gin.H{"error": "user with id#" + id + " not found"}
-		c.JSON(404, content)
+		c.JSON(404, gin.H{"error": "user not found"})
 	}
 	// curl -i http://localhost:8080/api/v1/users/1
 }
+
+
 func PostUser(c *gin.Context) {
-	// The futur code…
+	var user User
+	c.Bind(&user)
+	if user.Firstname != "" && user.Lastname != "" {
+		if insert, _ := dbmap.Exec(`INSERT INTO user (firstname, lastname) VALUES (?, ?)`, user.Firstname, user.Lastname); insert != nil {
+			user_id, err := insert.LastInsertId()
+			if err == nil {
+				content := &User{
+					Id: user_id,
+					Firstname: user.Firstname,
+					Lastname: user.Lastname,
+				}
+				c.JSON(201, content)
+			} else {
+				checkErr(err, "Insert failed")
+			}
+		}
+	} else {
+		c.JSON(422, gin.H{"error": "fields are empty"})
+	}
+	// curl -i -X POST -H "Content-Type: application/json" -d "{ \"firstname\": \"Thea\", \"lastname\": \"Queen\" }" http://localhost:8080/api/v1/users
 }
+
 func UpdateUser(c *gin.Context) {
-	// The futur code…
+	id := c.Params.ByName("id")
+	var user User
+	err := dbmap.SelectOne(&user, "SELECT * FROM user WHERE id=?", id)
+	if err == nil {
+		var json User
+		c.Bind(&json)
+		user_id, _ := strconv.ParseInt(id, 0, 64)
+		user := User{
+			Id: user_id,
+			Firstname: json.Firstname,
+			Lastname: json.Lastname,
+		}
+		if user.Firstname != "" && user.Lastname != ""{
+			_, err = dbmap.Update(&user)
+			if err == nil {
+				c.JSON(200, user)
+			} else {
+				checkErr(err, "Updated failed")
+			}
+		} else {
+			c.JSON(422, gin.H{"error": "fields are empty"})
+		}
+	} else {
+		c.JSON(404, gin.H{"error": "user not found"})
+	}
+	// curl -i -X PUT -H "Content-Type: application/json" -d "{ \"firstname\": \"Thea\", \"lastname\": \"Merlyn\" }" http://localhost:8080/api/v1/users/1
 }
+
 func DeleteUser(c *gin.Context) {
-	// The futur code…
+	id := c.Params.ByName("id")
+	var user User
+	err := dbmap.SelectOne(&user, "SELECT id FROM user WHERE id=?", id)
+	if err == nil {
+		_, err = dbmap.Delete(&user)
+		if err == nil {
+			c.JSON(200, gin.H{"id #" + id: " deleted"})
+		} else {
+			checkErr(err, "Delete failed")
+		}
+	} else {
+		c.JSON(404, gin.H{"error": "user not found"})
+	}
+	// curl -i -X DELETE http://localhost:8080/api/v1/users/1
+}
+var dbmap = initDb()
+
+func initDb() *gorp.DbMap {
+//	db, err := sql.Open("mysql", "root:mypassword@/myapi")
+	db, err := sql.Open("sqlite3", "./foo.db")
+	checkErr(err, "sql.Open failed")
+	dbmap := &gorp.DbMap{Db: db, Dialect:           gorp.SqliteDialect{}}
+		dbmap.AddTableWithName(User{}, "User").SetKeys(true, "Id")
+		err = dbmap.CreateTablesIfNotExists()
+		checkErr(err, "Create table failed")
+		return dbmap
+}
+
+func checkErr(err error, msg string) {
+	if err != nil {
+		log.Fatalln(msg, err)
+	}
 }
